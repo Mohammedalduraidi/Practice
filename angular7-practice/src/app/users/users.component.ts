9a0b0e7802d56d77fc374ebdda84aab193bc8f94
Mohammedalduraidi <mohd.alduraidi@gmail.com>
2018-11-06 16:57:20 +0200

    testing the life cycle hooks for angular

--- angular7-practice/src/app/users/users.component.ts
+++ angular7-practice/src/app/users/users.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterContentInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, OnDestroy, AfterContentInit, AfterViewInit {
+  constructor() {
+    console.log("Constructor")
+  }
 
-  constructor() { }
+  ngOnDestroy() {
+    console.log("OnDestroy");
+  }
+  ngAfterContentInit() {
+    console.log("AfterContentInit");
+  }
+  ngAfterViewInit() {
+    console.log("AfterViewInit");
+  }
+
 
   ngOnInit() {
+    console.log("OnInit");
   }
+
 
 }
